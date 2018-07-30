c68dfde5df9c7c7d8c908299fcf1b7c766d6b81d
wley3337 <wley3337@gmail.com>
2018-07-30 18:01:55 -0400

    card constructor

---
+++ src/cards.js
@@ -0,0 +1,9 @@
+class Card{
+    constructor(id, player_id, title, quote, moves){
+        this.id = id;
+        this.player_id = player_id;
+        this.title = title;
+        this.quote = quote;
+        this.moves = moves; //this is an array of moves
+    }
+}
